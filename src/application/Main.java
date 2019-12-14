76f77da2ebe5a9427082f0549ff0812aad6e68f5
Fernanda Uchôa Quirino <nanda.uq015@gmail.com>
2019-12-14 12:29:06 -0300

    Main View Controller

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,16 +4,15 @@ import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
+	public void start (Stage primaryStage) {
 		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/mainview.fxml")); //Instancia objeto loader Manipular a tela antes de carregar
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/mainview.fxml")); 
 			ScrollPane scrollpane = loader.load(); 
 			scrollpane.setFitToHeight(true);
 			scrollpane.setFitToWidth(true);

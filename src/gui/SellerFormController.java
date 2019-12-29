3e5d4bb037d68af77eaf94561cd0dfdfdfacf57a
Fernanda Uchôa Quirino <nanda.uq015@gmail.com>
2019-12-29 13:10:17 -0300

    textfield e datepicker

--- src/gui/SellerFormController.java
+++ src/gui/SellerFormController.java
@@ -1,35 +1,40 @@
 package gui;
 
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.Set;
 
-import javafx.event.ActionEvent;
 import db.DbException;
 import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Constraints;
 import gui.util.Utils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Seller;
+import model.entities.Seller;
 import model.exceptions.ValidationException;
+import model.services.SellerService;
 import model.services.SellerService;
 
 
 public class SellerFormController implements Initializable{
 	
 	private Seller entity;
-	
+
 	private SellerService service;
-	
+
 	private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
 	
 	@FXML
@@ -39,7 +44,25 @@ public class SellerFormController implements Initializable{
 	private TextField txtname;
 	
 	@FXML
+	private TextField txtEmail;
+
+	@FXML
+	private DatePicker dpBirthDate;
+
+	@FXML
+	private TextField txtBaseSalary;
+
+	@FXML
 	private Label labelErrorName;
+
+	@FXML
+	private Label labelErrorEmail;
+
+	@FXML
+	private Label labelErrorBirthDate;
+
+	@FXML
+	private Label labelErrorBaseSalary;
 	
 	@FXML
 	private Button btSave;
@@ -125,7 +148,10 @@ public class SellerFormController implements Initializable{
 	private void initializeNodes() {
 		
 		Constraints.setTextFieldInteger(txtid);
-		Constraints.setTextFieldMaxLength(txtname, 30);
+		Constraints.setTextFieldMaxLength(txtname, 70);
+		Constraints.setTextFieldDouble(txtBaseSalary);
+		Constraints.setTextFieldMaxLength(txtEmail, 60);
+		Utils.formatDatePicker(dpBirthDate, "dd/MM/yyyy");
 	}
 	
 	public void updateFormData() {
@@ -135,8 +161,13 @@ public class SellerFormController implements Initializable{
 		
 		txtid.setText(String.valueOf(entity.getId()));
 		txtname.setText(entity.getName());
+		txtEmail.setText(entity.getEmail());
+		txtBaseSalary.setText(String.format("%.2f", entity.getBaseSalary()));
+		if(entity.getBirthDate() != null) {
+			dpBirthDate.setValue(LocalDate.ofInstant(entity.getBirthDate().toInstant(), ZoneId.systemDefault()));
+	
+		}
 	}
-
 	private void setErrorMessages (Map<String, String> errors) {
 		Set<String> fields = errors.keySet();
 		
